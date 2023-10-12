f8241a7d5ed94daf90aa79ec15c6a02ab05a9021
oscardegroot <groot.oscar.de@gmail.com>
2023-10-12 13:09:53 +0000

    getshared package paht.

--- include/ros_tools/ros2_wrappers.h
+++ include/ros_tools/ros2_wrappers.h
@@ -8,6 +8,11 @@
 namespace RosTools
 {
 
+    inline std::string GetSharedPath(std::string &&package_name)
+    {
+        return ament_index_cpp::get_package_share_directory(package_name) + "/";
+    }
+
     inline std::string GetPackagePath(std::string &&package_name)
     {
         // Finds the share directory (ws/install/share/<package_name>)
